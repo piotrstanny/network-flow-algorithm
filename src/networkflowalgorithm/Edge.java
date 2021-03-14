d97088c48ec2ebca9e21fad3bb263753954590fe
Piotr Stanny <piotr.stanny@gmail.com>
2021-03-14 15:10:42 +0000

    Create Node and Edge class

---
+++ src/networkflowalgorithm/Edge.java
@@ -0,0 +1,8 @@
+package networkflowalgorithm;
+
+public class Edge {
+
+    private Integer capacity;
+    private Node from;
+    private Node to;
+}
---
+++ src/networkflowalgorithm/Node.java
@@ -0,0 +1,9 @@
+package networkflowalgorithm;
+
+public class Node {
+
+    private Integer nodeNumber;
+    private Integer maxFlowIn;
+    private Integer maxFlowOut;
+
+}

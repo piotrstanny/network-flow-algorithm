57c0bbe37edad9147abf389a5378a57173f5b882
Piotr Stanny <piotr.stanny@gmail.com>
2021-03-17 13:06:11 +0000

    Define Graph class attributes and constructor

--- src/networkflowalgorithm/Graph.java
+++ src/networkflowalgorithm/Graph.java
@@ -2,6 +2,15 @@ package networkflowalgorithm;
 
 public class Graph {
 
+    private Integer noOfNodes;
+    private Integer sourceNodeNumber = 0;
+    private Integer targetNodeNumber;
+    private NodeFrom[] arrayOfNodesFrom;
 
+    public Graph(Integer noOfNodes) {
+        this.noOfNodes = noOfNodes;
+        this.targetNodeNumber = noOfNodes - 1;
+        arrayOfNodesFrom = new NodeFrom[noOfNodes];
 
+    }
 }
